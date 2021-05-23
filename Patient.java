ddcd8093d3c0d77eb8264eb5b490378689cfbefc
dawolfe2 <79807784+dawolfe2@users.noreply.github.com>
2021-05-23 10:09:15 -0400

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,18 +1,26 @@
 package patient;
 
+    //Patient class for patient status and information
+    //patient variables are specific information related to their admission to the hosipital
 public class Patient {
 
+        //patient variables are made private to hide information
     private String first;
     private String last;
     private int age;
     private String sex;
+        //string describing the reason the patient is admitted
     private String illness;
+        //string describing all allergies of the patient
     private String allergies;
+        //date first admitted to the hospital
     private String dateAdmitted;
+        //hospital ward
     private String ward;
+        //room number
     private String room;
     
-    
+        //simple constructor creating patient object with all patient information parameters
     public Patient(String first, String last, int age, String sex, String illness, String allergies, String dateAdmitted, String ward, String room){
         
         this.first = first;
@@ -27,11 +35,14 @@ public class Patient {
         
     }
     
+        //toString method displaying all information
     public String toString(){
         return("Name: " + first + " " + last + "\nAge: " + age + "\nSex: " + sex + "\nReason for Admission: " + illness + "\nAllergies: " + allergies + 
                 "\nDate of Admission: " + dateAdmitted + "\nWard: " + ward + "\nroom: " + room);
     }
 
+        //getters and setters for all patient variables
+    
     public String getFirst() {
         return first;
     }
