c602b0305a28b60e056c198b8d01e5b453d6e32b
dawolfe2 <79807784+dawolfe2@users.noreply.github.com>
2021-05-29 13:40:58 -0400

    Update PatientLookupController.java

--- PatientLookupController.java
+++ PatientLookupController.java
@@ -3,6 +3,11 @@ package fx.test;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,6 +16,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
     
     //Daniel Wolfe
@@ -24,15 +31,96 @@ public class PatientLookupController implements Initializable {
     private Scene scene;
     private Parent root;
     
-        //button for entering patient name
+    @FXML
+    private TextField textName;
+    @FXML
+    private TextField textRoom;
+    @FXML
+    private TextArea textOut;
+    
+        //button for searching patient information by name
     @FXML
     private void handleButtonName(ActionEvent event) {
+        
+            //erases old text on button click
+        textOut.setText("");
+        String databaseURL = "jdbc:derby://localhost:1527/contact";
+        String last = textName.getText();
+        
+            //try catch for connecting to database
+        try {
+                //tries to connect to database using database URL, username and password parameter
+            Connection connection = DriverManager.getConnection(databaseURL, "nbuser", "nbuser");
+            System.out.println("Connected to Database");
+            
+                //searches for patient in database using lastname and returns result set of information
+            String sql = "SELECT * FROM Patients where lastname=?";
+            PreparedStatement p = connection.prepareStatement(sql);
+            p.setString(1, last);
+            ResultSet rs = p.executeQuery();
+            
+                //stores all obtained information in variables, creates new patient object with information and displays toString information to textbox
+            if (rs.next()) {
+                
+                String lastname = rs.getString("lastname");
+                String firstname = rs.getString("firstname");
+                int age = rs.getInt("age");
+                String sex = rs.getString("sex");
+                String illness = rs.getString("illness");
+                String allergies = rs.getString("allergies");
+                String dateAdmitted = rs.getString("dateadmitted");
+                String ward = rs.getString("ward");
+                int room = rs.getInt("roomnumber");
+                
+                Patient patient = new Patient(firstname, lastname, age, sex, illness, allergies, dateAdmitted, ward, room);
+                textOut.setText(patient.toString());
+            
+            }    
+                  
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
         
     }
     
         //button for entering patient room
+        //code is the same as above method but uses room number to search
     @FXML
     private void handleButtonRoom(ActionEvent event) {
+        
+        textOut.setText("");
+        String databaseURL = "jdbc:derby://localhost:1527/contact";
+        int roomnum = Integer.valueOf(textRoom.getText());
+        
+        try {
+            Connection connection = DriverManager.getConnection(databaseURL, "nbuser", "nbuser");
+            System.out.println("Connected to Database");
+            
+            String sql = "SELECT * FROM Patients where lastname=?";
+            PreparedStatement p = connection.prepareStatement(sql);
+            p.setInt(1, roomnum);
+            ResultSet rs = p.executeQuery();
+            
+            if (rs.next()) {
+                
+                String lastname = rs.getString("lastname");
+                String firstname = rs.getString("firstname");
+                int age = rs.getInt("age");
+                String sex = rs.getString("sex");
+                String illness = rs.getString("illness");
+                String allergies = rs.getString("allergies");
+                String dateAdmitted = rs.getString("dateadmitted");
+                String ward = rs.getString("ward");
+                int room = rs.getInt("roomnumber");
+                
+                Patient patient = new Patient(firstname, lastname, age, sex, illness, allergies, dateAdmitted, ward, room);
+                textOut.setText(patient.toString());
+                
+            }    
+                  
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
         
     }
     
